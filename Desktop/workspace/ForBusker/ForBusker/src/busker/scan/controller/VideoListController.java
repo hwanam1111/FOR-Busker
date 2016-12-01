1fbd79af9ede019ecfdbdad6687a0ca78baeffc3
user <user@DESKTOP-HK0IGCH>
2016-12-01 17:14:36 +0900

    대략적인 videoCollection 뷰 완성 (디비작업제외)

--- Desktop/workspace/ForBusker/ForBusker/src/busker/scan/controller/VideoListController.java
+++ Desktop/workspace/ForBusker/ForBusker/src/busker/scan/controller/VideoListController.java
@@ -1,10 +1,24 @@
 package busker.scan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 // 영상모음 컨트롤러
 
 @Controller
 public class VideoListController {
-
+	@RequestMapping(value="videoMain")
+	public String main(){
+		System.out.println("videoMain.jsp로 이동");
+		return "videoCollectionView/videoMain";
+	}
+	
+	@RequestMapping(value="videoView")
+	public String view(Model m){
+		
+		System.out.println("videoView.jsp로 이동");
+		//m.addAttribute("source","");
+		return "videoCollectionView/videoView";
+	}	
 }
