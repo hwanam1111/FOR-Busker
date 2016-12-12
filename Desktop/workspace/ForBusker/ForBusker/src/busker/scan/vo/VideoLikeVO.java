c8d27e31dc01652cad19454aa4c25c0132d38818
Lee Jun <hwanam1111@naver.com>
2016-12-12 18:19:20 +0900

    영상 좋아요 완료

---
+++ Desktop/workspace/ForBusker/ForBusker/src/busker/scan/vo/VideoLikeVO.java
@@ -0,0 +1,29 @@
+package busker.scan.vo;
+
+public class VideoLikeVO {
+
+	private int likeNo;
+	private String memLoginEmail;
+	private int videoNo;
+	
+	public int getLikeNo() {
+		return likeNo;
+	}
+	public void setLikeNo(int likeNo) {
+		this.likeNo = likeNo;
+	}
+	public String getMemLoginEmail() {
+		return memLoginEmail;
+	}
+	public void setMemLoginEmail(String memLoginEmail) {
+		this.memLoginEmail = memLoginEmail;
+	}
+	public int getVideoNo() {
+		return videoNo;
+	}
+	public void setVideoNo(int videoNo) {
+		this.videoNo = videoNo;
+	}
+	
+	
+}
